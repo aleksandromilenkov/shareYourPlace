ad3f77207e3ef50c4e877acafe5a14fe17c275b3
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-08-29 18:32:34 +0200

    Implementing Modal and Mapfor showing the location of the place on the map via OpenLayers maps

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,27 +1,53 @@
-import React from "react";
-import "./PlaceItem.css";
+import React, { useState } from "react";
 import Card from "../../shared/components/UIComponents/Card";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIComponents/Modal";
+import Map from "../../shared/components/UIComponents/Map";
+import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+  const openMapHandler = () => {
+    setShowMap(true);
+  };
+  const closeMapHandler = () => {
+    setShowMap(false);
+  };
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div class="place-item__image">
-          <img src={props.image} alt={props.title} />
+    <>
+      <Modal
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        onSubmit={closeMapHandler}
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+        show={showMap}
+        onCancel={closeMapHandler}
+      >
+        <div class="map-container">
+          <Map center={props.coordinates} zoom={16} />
         </div>
-        <div class="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div class="place-item__actions">
-          <Button inverse>VIEW ON MAP</Button>
-          <Button to={`/places/${props.id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div class="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div class="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div class="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
