eede75901423a7aac1e74fd5a5b2b9c3bb5468b5
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-08-29 21:16:21 +0200

    handling errors and validating the input in the Input components with validators utils functions

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
 import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Place enter a valid Title"
+      />
     </form>
   );
 };
