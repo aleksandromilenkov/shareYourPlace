04f7293b445fb459659e107524bd6ded40590934
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-10-03 20:58:31 +0200

    Creating custom Error model

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 
 const app = express();
 
 app.use(express.json({ limit: "10kb" }));
 
-app.use("/api/v1/places", placesRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+// special error handling middleware function by epxress (4 parameters)
+// this function will execute if any middleware before this function yielded an error
+app.use((error, req, res, next) => {
+  // if response is already thrown:
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500).json({
+    message: error.message || "Something went wrong",
+  });
+});
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,11 +1,62 @@
 const express = require("express");
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empile State",
+    description: "scy scraper big",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIP0NXvv5YbGKbuGsC425k3eh5ZX7y8dH5og&usqp=CAU",
+    address: "New York, New York, USA",
+    location: {
+      lat: 40.74854,
+      lng: -73.98732,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State",
+    description: "scy scraper big",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIP0NXvv5YbGKbuGsC425k3eh5ZX7y8dH5og&usqp=CAU",
+    address: "New York, New York, USA",
+    location: {
+      lat: 40.74854,
+      lng: -73.98732,
+    },
+    creator: "u2",
+  },
+];
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get("/:pid", (req, res, next) => {
   console.log("GET Request in Places");
+  const { pid } = req.params;
+  const place = DUMMY_PLACES.find((p) => p.id === pid);
+  if (!place) {
+    const error = new HttpError("Can't find place with that id.", 404);
+    error.code = 404;
+    return next(error);
+  }
   res.json({
-    message: "It works.",
+    status: "success",
+    data: place,
+  });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const { uid } = req.params;
+  const userPlaces = DUMMY_PLACES.filter((p) => p.creator == uid);
+  if (!userPlaces.length) {
+    const error = new HttpError("Can't find place created by that user.", 404);
+    return next(error);
+  }
+  res.status(200).json({
+    status: "success",
+    data: userPlaces,
   });
 });
 
