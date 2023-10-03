002d66ba6911e79f8bf9e08cf12b2c1d0864bdcd
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-10-03 22:06:27 +0200

    creating post route

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
+// parse the json body
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
+
+// Only run if no response was sent to the client
+app.use((req, res, next) => {
+  const error = new HttpError("404 Page not found", 404);
+  return next(error);
+});
 
 // special error handling middleware function by epxress (4 parameters)
 // this function will execute if any middleware before this function yielded an error
---
+++ backend/controllers/places-controller.js
@@ -0,0 +1,106 @@
+const HttpError = require("../models/http-error");
+const { v4: uuidv4 } = require("uuid");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empile State",
+    description: "scy scraper big",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIP0NXvv5YbGKbuGsC425k3eh5ZX7y8dH5og&usqp=CAU",
+    address: "New York, New York, USA",
+    location: {
+      lat: 40.74854,
+      lng: -73.98732,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State",
+    description: "scy scraper big",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIP0NXvv5YbGKbuGsC425k3eh5ZX7y8dH5og&usqp=CAU",
+    address: "New York, New York, USA",
+    location: {
+      lat: 40.74854,
+      lng: -73.98732,
+    },
+    creator: "u2",
+  },
+  {
+    id: "p2",
+    title: "Rockfeler Center",
+    description: "scy scraper big",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIP0NXvv5YbGKbuGsC425k3eh5ZX7y8dH5og&usqp=CAU",
+    address: "New York, New York, USA",
+    location: {
+      lat: 40.93233,
+      lng: -73.98732,
+    },
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  console.log("GET Request in Places");
+  const { pid } = req.params;
+  const place = DUMMY_PLACES.find((p) => p.id === pid);
+  if (!place) {
+    const error = new HttpError("Can't find place with that id.", 404);
+    error.code = 404;
+    return next(error);
+  }
+  res.json({
+    status: "success",
+    data: place,
+  });
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const { uid } = req.params;
+  const userPlaces = DUMMY_PLACES.filter((p) => p.creator === uid);
+  if (!userPlaces.length) {
+    const error = new HttpError("Can't find place created by that user.", 404);
+    return next(error);
+  }
+  res.status(200).json({
+    status: "success",
+    data: userPlaces,
+  });
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, imageUrl, address, location, creator } = req.body;
+  if (
+    !title ||
+    !description ||
+    !imageUrl ||
+    !address ||
+    !Object.keys(location).length ||
+    !creator
+  ) {
+    return next(new HttpError("Please provide all info", 401));
+  }
+  const newPlace = {
+    title,
+    description,
+    imageUrl,
+    address,
+    location,
+    creator,
+    id: uuidv4(),
+  };
+  DUMMY_PLACES.push(newPlace);
+  res.status(201).json({
+    status: "success",
+    data: newPlace,
+  });
+};
+
+module.exports = {
+  getPlaceById,
+  getPlacesByUserId,
+  createPlace,
+};
