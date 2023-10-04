d7d33123fa3d8eb1f33c5d0090be7866a40350f3
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-10-04 21:12:10 +0200

    creating routes and controller functions for the user routes

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,57 @@
+const { v4: uuidv4 } = require("uuid");
+const HttpError = require("../models/http-error");
+let DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Max",
+    password: "testest",
+    email: "maxasd@hee.com",
+  },
+  {
+    id: "u2",
+    name: "Manuel",
+    password: "testest2",
+    email: "manuelzxc@hee.com",
+  },
+];
+
+const getAllUsers = (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: DUMMY_USERS,
+  });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = DUMMY_USERS.find((u) => u.email === email);
+  if (user) {
+    const error = new HttpError("User with that e-mail already exists", 401);
+    return next(error);
+  }
+  const newUser = { id: uuidv4(), name, email, password };
+  DUMMY_USERS.push(newUser);
+  res.status(201).json({
+    status: "success",
+    data: newUser,
+  });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  const user = DUMMY_USERS.find((u) => u.email === email);
+  if (!user || user.password !== password) {
+    const error = new HttpError("Wrong email or password", 401);
+    return next(error);
+  }
+  res.status(200).json({
+    status: "success",
+    message: "logged in",
+  });
+};
+
+module.exports = {
+  getAllUsers,
+  signup,
+  login,
+};
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const {
+  getAllUsers,
+  login,
+  signup,
+} = require("../controllers/user-controller");
 
 const router = express.Router();
+
+router.get("/", getAllUsers);
+router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
