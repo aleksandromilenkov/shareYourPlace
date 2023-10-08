f1a29798456770fb68e478304cda1e3cc61893c1
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-10-08 21:39:09 +0200

    creating multer object and configuring limits and storage for signinup in the backend

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -5,12 +5,14 @@ const {
   login,
   signup,
 } = require("../controllers/user-controller");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").notEmpty(),
     check("email").normalizeEmail().isEmail(),
