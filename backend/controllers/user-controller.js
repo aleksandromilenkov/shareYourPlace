df65ff3904a0e39eaf2e14140ee9b3e6cc8a464f
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-10-09 22:28:35 +0200

    creating tokens for signup and login and returning them as response in the controller

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
 const User = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
 
 const getAllUsers = async (req, res, next) => {
   try {
@@ -39,9 +40,18 @@ const signup = async (req, res, next) => {
       places: [],
     });
 
+    let token;
+    // here you can sign something (usualy id and email)
+    token = jwt.sign(
+      { userId: newUser._id.toString(), email: newUser.email },
+      "secretjs",
+      { expiresIn: "1h" }
+    );
     res.status(201).json({
       status: "success",
-      data: newUser.toObject({ getters: true }),
+      token: token,
+      userId: newUser._id,
+      email: newUser.email,
     });
   } catch (err) {
     console.log(err);
@@ -61,10 +71,17 @@ const login = async (req, res, next) => {
       const error = new HttpError("Wrong email or password", 401);
       return next(error);
     }
+    const token = jwt.sign(
+      { email: user.email, userId: user._id.toString() },
+      "secretjs",
+      { expiresIn: "1h" }
+    );
     res.status(200).json({
       status: "success",
       message: "logged in",
-      data: user.toObject({ getters: true }),
+      token: token,
+      email: user.email,
+      userId: user._id,
     });
   } catch (err) {
     return next(new HttpError("Can not login right now", 500));
